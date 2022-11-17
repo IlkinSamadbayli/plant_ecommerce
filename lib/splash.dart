0cd3a9c6da1213a43d747f20c9e3bfabb5e75ce4
Hello Haystack <Hello Haystack@DESKTOP-K13HK9J>
2022-11-17 14:56:21 +0400

    add splash

---
+++ lib/splash.dart
@@ -0,0 +1,29 @@
+// import 'package:flutter/material.dart';
+// import 'package:plant_ecommerce/styles/colors/app_colors.dart';
+
+// class Splash extends StatelessWidget {
+//   const Splash({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       backgroundColor: AppColor.mainColor,
+//       body: Center(
+//         child: Container(
+//           width: 80,
+//           height: 80,
+//           decoration: BoxDecoration(
+//               shape: BoxShape.circle,
+//               border: Border.all(
+//                 width: 6,
+//                 color: AppColor.primaryColor,
+//                 strokeAlign: StrokeAlign.inside,
+//               )),
+//           child: Image.asset(
+//             "./assets/images/splash.png",
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
--- lib/ui/screens/login_page.dart
+++ lib/ui/screens/login_page.dart
@@ -56,164 +56,162 @@ class _LoginPageState extends State<LoginPage> {
         onTap: () {
           emailFocus.unfocus();
         },
-        child: Container(
-          child: Form(
-            key: formKey,
-            child: SingleChildScrollView(
-              child: Expanded(
-                child: Column(
-                  children: [
-                    const SizedBox(height: 80),
-                    Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 110),
-                      child: Image.asset(
-                        'assets/images/login_2.png',
-                        fit: BoxFit.fitWidth,
-                      ),
+        child: Form(
+          key: formKey,
+          child: SingleChildScrollView(
+            child: Expanded(
+              child: Column(
+                children: [
+                  const SizedBox(height: 80),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 110),
+                    child: Image.asset(
+                      'assets/images/login_2.png',
+                      fit: BoxFit.fitWidth,
                     ),
-                    const SizedBox(height: 30),
-                    Text(
-                      "Login to Your Account",
-                      style: CustomTextStyle.standardStyle,
-                    ),
-                    const SizedBox(height: 16),
-                    Column(children: [
-                      GlobalInput(
-                        labelText: "Your email",
-                        controller: mailController,
-                        isPassword: false,
-                        validator: emailValidator,
-                        prefixIcon: const Icon(Icons.mail),
-                        isCorrect: isTrueMail,
-                        onChanged: (mail) {
-                          setState(() {});
-                          if (!mail.contains("@") ||
-                              !RegExp(r'^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$')
-                                  .hasMatch(mail)) {
-                            isTrueMail = false;
-                          } else {
-                            isTrueMail = true;
-                          }
-                        },
-                        textFocus: emailFocus,
-                        keyboardType: TextInputType.emailAddress,
-                      ),
-                      GlobalInput(
-                        labelText: "Your password",
-                        controller: passwordController,
-                        isPassword: true,
-                        validator: passwordValidator,
-                        prefixIcon: const Icon(Icons.lock),
-                        isCorrect: isTruePassword,
-                        onChanged: (password) {
-                          setState(() {});
-                          if (password.length < 8 ||
-                              !RegExp(r'^[A-Z a-z 0-9]+$').hasMatch(password)) {
-                            isTruePassword = false;
-                          } else {
-                            isTruePassword = true;
-                          }
-                        },
-                        textFocus: passwordFocus,
-                        keyboardType: TextInputType.text,
-                      ),
-                    ]),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Checkbox(
-                          shape: const CircleBorder(),
-                          activeColor: AppColor.primaryColor,
-                          value: isChecked,
-                          onChanged: (value) {
-                            setState(() {
-                              isChecked = !isChecked;
-                            });
-                          },
-                        ),
-                        Text(
-                          'Remember me',
-                          style: CustomTextStyle.tinyStyleItalic,
-                        ),
-                      ],
-                    ),
-                    globalButton(
-                      text: 'Sign in',
-                      clicked: true,
-                      onTap: () {
-                        if (formKey.currentState!.validate()) {
-                          GlobalRoutes.to(context, const AccountSetup());
+                  ),
+                  const SizedBox(height: 30),
+                  Text(
+                    "Login to Your Account",
+                    style: CustomTextStyle.standardStyle,
+                  ),
+                  const SizedBox(height: 16),
+                  Column(children: [
+                    GlobalInput(
+                      labelText: "Your email",
+                      controller: mailController,
+                      isPassword: false,
+                      validator: emailValidator,
+                      prefixIcon: const Icon(Icons.mail),
+                      isCorrect: isTrueMail,
+                      onChanged: (mail) {
+                        setState(() {});
+                        if (!mail.contains("@") ||
+                            !RegExp(r'^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$')
+                                .hasMatch(mail)) {
+                          isTrueMail = false;
                         } else {
-                          GlobalRoutes.to(context, const AccountSetup());
-
-                          context.snackbarErrorMessage;
+                          isTrueMail = true;
                         }
                       },
-
-                      // GlobalRoutes.to(context, const AccountSetup());
-                      isIcon: false,
+                      textFocus: emailFocus,
+                      keyboardType: TextInputType.emailAddress,
                     ),
-                    const SizedBox(height: 32),
-                    InkWell(
-                      child: Text(
-                        'Forgot the password?',
+                    GlobalInput(
+                      labelText: "Your password",
+                      controller: passwordController,
+                      isPassword: true,
+                      validator: passwordValidator,
+                      prefixIcon: const Icon(Icons.lock),
+                      isCorrect: isTruePassword,
+                      onChanged: (password) {
+                        setState(() {});
+                        if (password.length < 8 ||
+                            !RegExp(r'^[A-Z a-z 0-9]+$').hasMatch(password)) {
+                          isTruePassword = false;
+                        } else {
+                          isTruePassword = true;
+                        }
+                      },
+                      textFocus: passwordFocus,
+                      keyboardType: TextInputType.text,
+                    ),
+                  ]),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Checkbox(
+                        shape: const CircleBorder(),
+                        activeColor: AppColor.primaryColor,
+                        value: isChecked,
+                        onChanged: (value) {
+                          setState(() {
+                            isChecked = !isChecked;
+                          });
+                        },
+                      ),
+                      Text(
+                        'Remember me',
                         style: CustomTextStyle.tinyStyleItalic,
                       ),
+                    ],
+                  ),
+                  globalButton(
+                    text: 'Sign in',
+                    clicked: true,
+                    onTap: () {
+                      if (formKey.currentState!.validate()) {
+                        GlobalRoutes.to(context, const AccountSetup());
+                      } else {
+                        GlobalRoutes.to(context, const AccountSetup());
+
+                        context.snackbarErrorMessage;
+                      }
+                    },
+
+                    // GlobalRoutes.to(context, const AccountSetup());
+                    isIcon: false,
+                  ),
+                  const SizedBox(height: 32),
+                  InkWell(
+                    child: Text(
+                      'Forgot the password?',
+                      style: CustomTextStyle.tinyStyleItalic,
                     ),
-                    const SizedBox(height: 16),
-                    Text(
-                      'or continue with',
-                      style: CustomTextStyle.tinyStyleGray,
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.all(24),
-                      child: Row(
-                        children: [
-                          socialNetworkRow(
-                            widget: Icon(
-                              Icons.facebook_rounded,
-                              color: AppColor.focusColor,
-                              size: 36,
-                            ),
-                            onTap: () {},
-                          ),
-                          socialNetworkRow(
-                            widget: Image.asset(
-                              './assets/images/google.png',
-                            ),
-                            onTap: () {},
-                          ),
-                          socialNetworkRow(
-                            widget: Icon(
-                              Icons.apple,
-                              size: 36,
-                              color: AppColor.textColor,
-                            ),
-                            onTap: () {},
-                          ),
-                        ],
-                      ),
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
+                  ),
+                  const SizedBox(height: 16),
+                  Text(
+                    'or continue with',
+                    style: CustomTextStyle.tinyStyleGray,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(24),
+                    child: Row(
                       children: [
-                        Text('Dont have an account?',
-                            style: CustomTextStyle.tinyStyleItalic),
-                        const SizedBox(width: 4),
-                        InkWell(
-                          onTap: () {
-                            GlobalRoutes.to(context, const SignUp());
-                          },
-                          child: Text(
-                            'Sign up',
-                            style: CustomTextStyle.tinyStyleBold
-                                .copyWith(color: AppColor.primaryColor),
+                        socialNetworkRow(
+                          widget: Icon(
+                            Icons.facebook_rounded,
+                            color: AppColor.focusColor,
+                            size: 36,
                           ),
-                        )
+                          onTap: () {},
+                        ),
+                        socialNetworkRow(
+                          widget: Image.asset(
+                            './assets/images/google.png',
+                          ),
+                          onTap: () {},
+                        ),
+                        socialNetworkRow(
+                          widget: Icon(
+                            Icons.apple,
+                            size: 36,
+                            color: AppColor.textColor,
+                          ),
+                          onTap: () {},
+                        ),
                       ],
                     ),
-                  ],
-                ),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text('Dont have an account?',
+                          style: CustomTextStyle.tinyStyleItalic),
+                      const SizedBox(width: 4),
+                      InkWell(
+                        onTap: () {
+                          GlobalRoutes.to(context, const SignUp());
+                        },
+                        child: Text(
+                          'Sign up',
+                          style: CustomTextStyle.tinyStyleBold
+                              .copyWith(color: AppColor.primaryColor),
+                        ),
+                      )
+                    ],
+                  ),
+                ],
               ),
             ),
           ),
