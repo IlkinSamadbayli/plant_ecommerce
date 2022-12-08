54313eb33842ff585d78c40c66136191f737db32
Hello Haystack <Hello Haystack@DESKTOP-K13HK9J>
2022-12-09 00:11:42 +0400

    Find errors

--- lib/presentation/screens/home/components/provider/home_provider.dart
+++ lib/presentation/screens/home/components/provider/home_provider.dart
@@ -1,17 +1,123 @@
 import 'package:flutter/material.dart';
+import 'package:plant_ecommerce/global/global_assets/global_assets.dart';
+import 'package:plant_ecommerce/model/product_model.dart';
 
 class HomeProvider extends ChangeNotifier {
-  bool isLiked = false;
   bool isNotificationOpen = false;
+  List<ProductModel> favorCards = [];
+
+  void addFavor(ProductModel item) {
+    item.isFavorite = !item.isFavorite;
+    if (item.isFavorite) {
+      favorCards.add(item);
+    } else if (item.isFavorite == false) {
+      favorCards.remove(item);
+    }
+    notifyListeners();
+  }
 
   void get notificationAppbar {
     isNotificationOpen = !isNotificationOpen;
-
     notifyListeners();
   }
 
-  void get likedAppbarHome {
-    isLiked = !isLiked;
-    notifyListeners();
-  }
+
+  List<ProductModel> productData_1 = [
+    ProductModel(
+      title: "Chinese Money",
+      image: Image.asset(
+        GlobalAssets.card_1,
+        fit: BoxFit.fill,
+      ),
+      price: 12,
+      rated: '3.8',
+      sold: '443',
+    ),
+    ProductModel(
+      title: "Yucca",
+      image: Image.asset(
+        GlobalAssets.card_2,
+        fit: BoxFit.fill,
+      ),
+      price: 10,
+      rated: '4.1',
+      sold: '176',
+    ),
+    ProductModel(
+      title: "Monstera",
+      image: Image.asset(
+        GlobalAssets.card_3,
+        fit: BoxFit.fill,
+      ),
+      price: 9,
+      rated: '4.3',
+      sold: '51',
+    ),
+  ];
+
+  List<ProductModel> productData_2 = [
+    ProductModel(
+      title: "Aloe Vera",
+      image: Image.asset(
+        GlobalAssets.card_4,
+        fit: BoxFit.fill,
+      ),
+      price: 16,
+      rated: '4.1',
+      sold: '57',
+    ),
+    ProductModel(
+      title: "Spider Plant",
+      image: Image.asset(
+        GlobalAssets.card_5,
+        fit: BoxFit.fill,
+      ),
+      price: 11,
+      rated: '4.0',
+      sold: '147',
+    ),
+    ProductModel(
+      title: "Dumbcane",
+      image: Image.asset(
+        GlobalAssets.card_6,
+        fit: BoxFit.fill,
+      ),
+      price: 7,
+      rated: '3.3',
+      sold: '54',
+    ),
+  ];
+
+  List<ProductModel> productData_3 = [
+    ProductModel(
+      title: "Orchid",
+      image: Image.asset(
+        GlobalAssets.card_7,
+        fit: BoxFit.fill,
+      ),
+      price: 14,
+      rated: '3.9',
+      sold: '412',
+    ),
+    ProductModel(
+      title: "Bromeliad",
+      image: Image.asset(
+        GlobalAssets.card_8,
+        fit: BoxFit.fill,
+      ),
+      price: 11,
+      rated: '3.6',
+      sold: '165',
+    ),
+    ProductModel(
+      title: "Lucky Bamboo",
+      image: Image.asset(
+        GlobalAssets.card_9,
+        fit: BoxFit.fill,
+      ),
+      price: 10,
+      rated: '4.1',
+      sold: '128',
+    ),
+  ];
 }
